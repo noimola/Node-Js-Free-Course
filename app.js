3247d227b895c49ee3e7e74fd61ca762fa629349
noimola <113587899+noimola@users.noreply.github.com>
2023-02-22 09:37:28 +0700

    add all file of project
    hello wold
    
    Co-Authored-By: noimola <113587899+noimola@users.noreply.github.com>

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send('Hello Parn');
+});
+
+app.use(morgan('combined'));
+
+app.listen(port, () => {
+    debug("Test port" + chalk.red(port));
+});
