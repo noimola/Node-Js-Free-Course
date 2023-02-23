70cc36c20dff53197fec8b55c283524ef066e3d5
noimola <113587899+noimola@users.noreply.github.com>
2023-02-23 15:32:19 +0700

    now
    Co-Authored-By: noimola <113587899+noimola@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const chalk = require('chalk');
-const debug = require('debug');
+const debug = require('debug')('app');
 const morgan = require('morgan');
-const app = express();
-const port = 3000;
+const path = require('path');
 
-app.get("/", (req, res) => {
-    res.send('Hello Parn Sud xso');
-});
+const app = express();
+const PORT = process.env.PORT || 4000;
+const productsRouter = require("./src/router/productsRouter");
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/public/")));
 
-app.listen(port, () => {
-    debug("Test port" + chalk.red(port));
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+
+app.use("/products", productsRouter);
+
+app.get("/", (req, res) => {
+    res.render('index', { username: 'สุดเท่', customer: ["A", "B", "C"] });
+});
+
+app.listen(PORT, () => {
+    debug("Test port" + chalk.red(PORT));
 });
